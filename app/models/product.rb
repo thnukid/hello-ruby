13abd9be285af9df4312623889589fa94b75a3b7
Eric Claus Bartholemy <ericclaus@kabisa.nl>
2014-08-05 11:24:29 +0200

    add length check on title and custom error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length:{maximum: 10, message: 'can not be longer than 10 letters'}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image'
