062b60a7de732fa321abb0123644e9f4c90d5a5f
Eric Claus Bartholemy <ericclaus@kabisa.nl>
2014-08-06 12:10:22 +0200

    Iteration F4 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+  def hidden_div_if(condition, attributes = {},&block)
+    if condition
+        attributes["style"] = "display:none"
+    end
+    content_tag("div",attributes,&block)
+  end
+
+  #playtime
   def store_visited
    session[:counter]
    end
