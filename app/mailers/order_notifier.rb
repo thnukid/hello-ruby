ae1e2cc7a3416a5e8dc69a73ddc81bb5b700e286
Eric Claus Bartholemy <ericclaus@kabisa.nl>
2014-08-07 12:08:57 +0200

    Iteration H1 Email Support

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,36 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "Eric Ruby <kab@example.com>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
+
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
+  end
+
+end
